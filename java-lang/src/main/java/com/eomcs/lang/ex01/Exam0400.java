0323a8d6bc9df8efcfdb390a90aee6d62f9eb423
trev0129 <pbm9393@gmail.com>
2022-07-05 23:08:59 +0900

    .

--- java-lang/src/main/java/com/eomcs/lang/ex01/Exam0400.java
+++ java-lang/src/main/java/com/eomcs/lang/ex01/Exam0400.java
@@ -15,8 +15,8 @@ package com.eomcs.lang.ex01;
 //
 
 public class Exam0400 {
-  //  public static void main(String[] args) {
-  //  }
+    public static void main(String[] args) {
+    }
 }
 
 //## 실습
