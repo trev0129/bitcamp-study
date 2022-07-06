cef38657fbd5de6bf98148890f3552a641cac690
trev0129 <pbm9393@gmail.com>
2022-07-06 17:17:32 +0900

    .

--- java-lang/src/main/java/Exam01.java
+++ java-lang/src/main/java/Exam01.java
@@ -1,3 +1,5 @@
+package a.b.c.d;
+
 class A {}
 class B {}
 class C {}
--- java-lang/src/main/java/Exam03.java
+++ java-lang/src/main/java/Exam03.java
@@ -1,3 +1,4 @@
+package x.y.z;
 public class Exam03 {
   public static void main(String[] args) {
     System.out.println("Hello!");
