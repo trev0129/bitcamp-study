dca2b8b1b08220fd3bc0c27bfb0e3266819f1ffc
trev0129 <pbm9393@gmail.com>
2022-07-11 11:08:13 +0900

    .

--- board-app/app/src/main/java/com/bitcamp/board/App.java
+++ board-app/app/src/main/java/com/bitcamp/board/App.java
@@ -14,49 +14,48 @@ public class App {
 
     java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
     while (true) {
-    System.out.println("메뉴:");
-    System.out.println("  1: 게시글 목록");
-    System.out.println("  2: 게시글 상세보기");
-    System.out.println();
-    System.out.print("메뉴를 선택하세요. [1..2](0: 종료) ");
+      System.out.println("메뉴:");
+      System.out.println("  1: 게시글 목록");
+      System.out.println("  2: 게시글 상세보기");
+      System.out.println();
+      System.out.print("메뉴를 선택하세요. [1..2](0: 종료) ");
 
-    int menuNo = keyboardInput.nextInt();
-    keyboardInput.nextLine(); //입력한 숫자 뒤에 남아있는 줄바꿈 코드 제거
+      int menuNo = keyboardInput.nextInt();
+      keyboardInput.nextLine(); //입력한 숫자 뒤에 남아있는 줄바꿈 코드 제거
 
-    if (menuNo == 0) {
-      break;
-    } else if (menuNo == 1) {
-      System.out.println("[게시글 목록]");
-    System.out.println("번호 제목 조회수 작성자 등록일");
-    System.out.print(1);
-    System.out.print("\t");
-    System.out.print("제목입니다.1");
-    System.out.print('\t');
-    System.out.print(20 + "\t");
-    System.out.print("홍길동\t");
-    System.out.print("2022-07-08\n");
-    System.out.print(2 + "\t" + "제목입니다.2" + '\t' + 11 + 
-      "\t" + "홍길동\t" + "2022-07-08\n");
+      if (menuNo == 0) {
+        break;
+      } else if (menuNo == 1) {
+        System.out.println("[게시글 목록]");
+        System.out.println("번호 제목 조회수 작성자 등록일");
+        System.out.print(1);
+        System.out.print("\t");
+        System.out.print("제목입니다.1");
+        System.out.print('\t');
+        System.out.print(20 + "\t");
+        System.out.print("홍길동\t");
+        System.out.print("2022-07-08\n");
+        System.out.print(2 + "\t" + "제목입니다.2" + '\t' + 11 + 
+        "\t" + "홍길동\t" + "2022-07-08\n");
 
-    System.out.println(3 + "\t제목입니다.3\t" + 31 + 
-      "\t" + "홍길동\t" + "2022-07-08");
+        System.out.println(3 + "\t제목입니다.3\t" + 31 + 
+        "\t" + "홍길동\t" + "2022-07-08");
     
-    System.out.printf("%d\t%s\t%d\t%s\t%s\n", 
-      4, "제목입니다.4", 45, "홍길동", "2022-07-08");
-    } else if (menuNo == 2) {
-      System.out.println("[게시판 상세보기]");
+        System.out.printf("%d\t%s\t%d\t%s\t%s\n", 
+        4, "제목입니다.4", 45, "홍길동", "2022-07-08");
+      } else if (menuNo == 2) {
+        System.out.println("[게시판 상세보기]");
 
-    System.out.printf("번호: %d\n", 1);
-    System.out.printf("제목: %s\n", "제목입니다.");
-    System.out.printf("내용: %s\n", "내용입니다");
-    System.out.printf("조회수: %d\n", 100);
-    System.out.printf("작성자: %s\n", "홍길동");
-    System.out.printf("등록일: %s\n", "2022-07-08");
-    
-    } else {
-      System.out.println("1에서 2사이의 메뉴 번호를 입력하세요. ");
-    }
+        System.out.printf("번호: %d\n", 1);
+        System.out.printf("제목: %s\n", "제목입니다.");
+        System.out.printf("내용: %s\n", "내용입니다");
+        System.out.printf("조회수: %d\n", 100);
+        System.out.printf("작성자: %s\n", "홍길동");
+        System.out.printf("등록일: %s\n", "2022-07-08");
+      } else {
+        System.out.println("1에서 2사이의 메뉴 번호를 입력하세요. ");
+      }
     } //while문 종료 
     System.out.println("안녕히가세요!");
-  }
+  } 
 }
