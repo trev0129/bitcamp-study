6b0194b3b58be56c55237ff05325c5ab0b3958a9
trev0129 <pbm9393@gmail.com>
2022-10-24 21:36:45 +0900

    .

--- eomcs-web/app/src/main/java/com/eomcs/web/App.java
+++ eomcs-web/app/src/main/java/com/eomcs/web/App.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class App {
 
   public static void main(String[] args) {
+    System.out.println("Server Start!!");
     SpringApplication.run(App.class, args);
   }
 
